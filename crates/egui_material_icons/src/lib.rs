9c1a4b5e7b4c0878e77e509cbd5687e4e2b9c5af
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2026-02-28 16:53:48 +0000

    Remove egui icons y offset

--- crates/egui_material_icons/src/lib.rs
+++ crates/egui_material_icons/src/lib.rs
@@ -10,7 +10,6 @@ pub const FONT_DATA: &[u8] = include_bytes!("../MaterialSymbolsRounded_Filled-Re
 
 pub fn font_insert() -> FontInsert {
     let mut data = FontData::from_static(FONT_DATA);
-    data.tweak.y_offset_factor = 0.05;
 
     FontInsert::new(
         "material-icons",
